e038592a5cee5c82dbb8ea5eb53a7553727538a6
ambamxlc <101145609+ambamxlc@users.noreply.github.com>
2022-05-31 09:48:43 -0400

    Add files via upload

---
+++ demo/spring/src/main/java/com/spring/spring/Greeting.java
@@ -0,0 +1,19 @@
+package com.spring.spring;
+
+public class Greeting {
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
